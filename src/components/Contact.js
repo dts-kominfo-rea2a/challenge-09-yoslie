6df33c858053a0eae13bc4fbd71d2914873728c3
Yoslie <yoslie.tan@gmail.com>
2022-06-26 22:32:15 +0700

    feat: create contact list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div className='contact-list'>
+            <div className='contact-image'>
+                <img className='contact-img' src={data.photo} alt={data.name} />
+            </div>
+            <div className='contact-detail'>
+                <div className='contact-name'>{ data.name || '' }</div>
+                <div className='contact-phone'>{ data.phone || '' }</div>
+                <div className='contact-email'>{ data.email || '' }</div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <h1>Call a friend</h1>
+            <p>your friendly contact app</p>
+        </>
     )
 }
 
